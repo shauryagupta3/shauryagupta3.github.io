00a0fbc67f01e6e75164a90ca92d1370456ca1aa
shaurya <119103602+shauryagupta3@users.noreply.github.com>
2024-02-06 20:44:18 +0530

    changed desc for projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Image from "next/image";
 import Link from "next/link";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
+import { FileIcon } from "lucide-react";
 
 async function getAvatarUrl() {
   const response = await fetch("https://api.github.com/users/shauryagupta3")
@@ -9,6 +11,50 @@ async function getAvatarUrl() {
     .then((res) => res.avatar_url);
   return response;
 }
+
+interface project {
+  title: string;
+  code_link: string;
+  repo_link: string;
+  img_url: string;
+  description: string;
+}
+
+const projects: project[] = [
+  {
+    title: "Weather App",
+    code_link: "https://shauryagupta3.github.io",
+    repo_link: "https://github.com/shauryagupta3/weather-app",
+    img_url: "/static/images/weather-app",
+    description:
+      "A weather forecast app made with React which uses Open weather api for fetching data. Implemented pagination in search bar for better user experience. Gives user detailed information about current weather along with forecast for upcoming week.",
+  },
+  {
+    title: "Lift Simulation",
+    code_link: "https://shauryagupta3.github.io/lift-simulation-js/",
+    repo_link: "https://github.com/shauryagupta3/lift-simulation-js",
+    img_url: "/static/images/lift-simulation",
+    description:
+      "A visual simulation of how lift systems works doing calls to system. Maintaining queues for management of lifts and vivid animations for better visual representation.",
+  },
+  {
+    title: "BlackJack",
+    code_link: "https://blackjack-js.pages.dev/",
+    repo_link: "https://github.com/shauryagupta3/blackjack-game",
+    img_url: "/static/images/lift-simulation",
+    description:
+      "Classic blackjack game implemented with visual cards to be played. Also added a casino like betting system if player wins bet is 2x else bet is gone.",
+  },
+  {
+    title: "The Protein Project",
+    code_link: "",
+    repo_link: "https://github.com/shauryagupta3/Protein-Project",
+    img_url:
+      "https://raw.githubusercontent.com/shauryagupta3/Protein-Project/main/img/screenshot.jpeg",
+    description:
+      "The Protein Project is a powerful application designed to help you discover the most optimal combination of foods that maximize your protein intake while adhering to your specified calorie limit.We've harnessed the potential of fast-food dataset from USA, which we've supplemented with manually added entries to enhance the variety of our recommendations.",
+  },
+];
 
 export default async function Home() {
   const avatar_url = await getAvatarUrl();
@@ -26,6 +72,18 @@ export default async function Home() {
               <h1 className="text-5xl font-bold">Hi, I am</h1>
               <h1 className="text-7xl text-indigo-400 font-bold">Shaurya</h1>
               <h1 className="text-xl font-semibold">I code</h1>
+              <div className="flex justify-start gap-2">
+                <Button variant={"outline"} size={"icon"} asChild>
+                  <Link href={"https://github.com/shauryagupta3"}>
+                    <GitHubLogoIcon className="h-6 w-6" />
+                  </Link>
+                </Button>
+                <Button variant={"outline"} size={"icon"} asChild>
+                  <Link href={"/static/cv_wo_ph"}>
+                    <FileIcon className="h-6 w-6" />
+                  </Link>
+                </Button>
+              </div>
             </div>
           </div>
           <div
@@ -56,112 +114,37 @@ export default async function Home() {
           {" "}
           <h1 className="text-4xl font-bold">Projects</h1>
           <div className="my-4 flex flex-col w-full gap-2">
-            <div className="flex flex-col text-center sm:text-start sm:flex-row p-2 justify-between border rounded-xl border-zinc-500">
-              <div className="sm:w-1/2 flex-col h-full justify-between flex">
-                <h1 className="text-3xl font-semibold">Weather App</h1>
-                <div className="flex justify-center sm:justify-start py-2 gap-2">
-                  <Button asChild variant={"secondary"}>
-                    <Link href={"https://weather-app-react-a32.pages.dev/"}>
-                      Live
-                    </Link>
-                  </Button>
-                  <Button asChild variant={"secondary"}>
-                    <Link href={"https://github.com/shauryagupta3/weather-app"}>
-                      Code
-                    </Link>
-                  </Button>
+            {projects.map((e, i) => (
+              <div
+                key={i}
+                className="flex flex-col text-center sm:text-start sm:flex-row p-2 justify-between border rounded-xl border-zinc-500"
+              >
+                <div className="sm:w-1/2 flex-col h-full justify-between flex">
+                  <h1 className="text-3xl font-semibold">{e.title}</h1>
+                  <div className="flex justify-center sm:justify-start py-2 gap-2">
+                    {e.code_link != "" ? (
+                      <Button asChild variant={"secondary"}>
+                        <Link href={e.code_link}>Live</Link>
+                      </Button>
+                    ) : (
+                      ""
+                    )}
+                    <Button asChild variant={"secondary"}>
+                      <Link href={e.repo_link}>Code</Link>
+                    </Button>
+                  </div>
+                  <p>{e.description}</p>
                 </div>
-                <p>
-                  A weather forecast app made with React which uses Open weather
-                  api to get data for fetching data. It comes with auto-complete
-                  for better user experience.
-                </p>
-              </div>
-              <div className="self-center">
-                <Image
-                  src={`/static/images/weather-app.png`}
-                  alt="weather app image"
-                  height={100}
-                  width={300}
-                />
-              </div>
-            </div>{" "}
-            <div className="flex flex-col text-center sm:text-start sm:flex-row p-2 justify-between border rounded-xl border-zinc-500">
-              <div className="sm:w-1/2 flex-col h-full justify-between flex">
-                <h1 className="text-3xl font-semibold">Lift Simulation</h1>
-                <div className="flex justify-center sm:justify-start py-2 gap-2">
-                  <Button asChild variant={"secondary"}>
-                    <Link
-                      href={
-                        "https://shauryagupta3.github.io/lift-simulation-js/"
-                      }
-                    >
-                      Live
-                    </Link>
-                  </Button>
-                  <Button asChild variant={"secondary"}>
-                    <Link
-                      href={
-                        "https://github.com/shauryagupta3/lift-simulation-js"
-                      }
-                    >
-                      Code
-                    </Link>
-                  </Button>
+                <div className="self-center">
+                  <Image
+                    src={e.img_url}
+                    alt="weather app image"
+                    height={100}
+                    width={300}
+                  />
                 </div>
-                <p>
-                  A visual simulation of how lift systems works doing calls to
-                  system. Maintaining queues for management of lifts and vivid
-                  animations for better visual representation. This have dynamic
-                  layout user can input number of lifts and floors they want.
-                </p>
               </div>
-              <div className="self-center">
-                <Image
-                  src={`/static/images/lift-simulation.png`}
-                  alt="weather app image"
-                  height={100}
-                  width={300}
-                />
-              </div>
-            </div>{" "}
-            <div className="flex flex-col text-center sm:text-start sm:flex-row p-2 justify-between border rounded-xl border-zinc-500">
-              <div className="sm:w-1/2 flex-col h-full justify-between flex">
-                <h1 className="text-3xl font-semibold">BlackJack</h1>
-                <div className="flex justify-center sm:justify-start py-2 gap-2">
-                  <Button asChild variant={"secondary"}>
-                    <Link href={"https://blackjack-js.pages.dev/"}>Live</Link>
-                  </Button>
-                  <Button asChild variant={"secondary"}>
-                    <Link
-                      href={"https://github.com/shauryagupta3/blackjack-game"}
-                    >
-                      Code
-                    </Link>
-                  </Button>
-                </div>
-                <p>
-                  Classic blackjack game implemented with visual cards to be
-                  played.{" "}
-                  <a
-                    href="https://www.wikihow.com/Play-Blackjack"
-                    className="text-blue-600 underline"
-                  >
-                    How to play
-                  </a>
-                  . Also added a casino like betting system if player wins bet
-                  is 2x else bet is gone.
-                </p>
-              </div>
-              <div className="self-center">
-                <Image
-                  src={`/static/images/blackjack.png`}
-                  alt="weather app image"
-                  height={100}
-                  width={300}
-                />
-              </div>
-            </div>{" "}
+            ))}
           </div>
         </div>
         <div
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,114 +29,137 @@ const linksInNav: { title: string; href: string; description: string }[] = [
     href: "https://github.com/shauryagupta3",
     description: "Github profile",
   },
+  {
+    title: "Resume",
+    href: "/static/cv_wo_ph",
+    description: "Latest resume",
+  },
 ];
 
-const linksInNavMobile : {title:string,href:string}[] = [
-{
-  title:"Projects",
-  href:"/#projects"
-},
-{
-  title:"About",
-  href:"/#about"
-},
-{
-  title:"Blog",
-  href:"/blog"
-},
-{
-  title:"Linkedin",
-  href:"https://in.linkedin.com/in/shauryagupta3gg"
-},
-{
-  title:"Github",
-  href:"https://github.com/shauryagupta3"
-},
-]
+const linksInNavMobile: { title: string; href: string }[] = [
+  {
+    title: "Projects",
+    href: "/#projects",
+  },
+  {
+    title: "About",
+    href: "/#about",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Linkedin",
+    href: "https://in.linkedin.com/in/shauryagupta3gg",
+  },
+  {
+    title: "Github",
+    href: "https://github.com/shauryagupta3",
+  },
+];
 
 export default function Navbar() {
-const [visible,setVisible] = React.useState(true)
-const [prevScrollPos, setPrevScrollPos] = React.useState(0)
-const [show,setShow] = React.useState(false)
+  const [visible, setVisible] = React.useState(true);
+  const [prevScrollPos, setPrevScrollPos] = React.useState(0);
+  const [show, setShow] = React.useState(false);
 
-const handleScroll = () => {
-    const currentScrollPos = window.scrollY
-    if(currentScrollPos > prevScrollPos){
-        setVisible(false)
-    }else{
-        setVisible(true)
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+    if (currentScrollPos > prevScrollPos) {
+      setVisible(false);
+    } else {
+      setVisible(true);
     }
 
-    setPrevScrollPos(currentScrollPos)
-}
+    setPrevScrollPos(currentScrollPos);
+  };
 
-React.useEffect( () => {
-    window.addEventListener('scroll', handleScroll);
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll)
-})
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
 
-  return (<nav className={`w-full flex justify-center sticky z-50 ${visible ? 'top-0' : ''} bg-white dark:bg-zinc-950 transition duration-1000 ease-in-out`}>
-    <div className="flex justify-between w-[95%] m-4 p-2 items-center border rounded-lg border-zinc-600">
-      <Link href={"/"} className="text-xl">
-        <h2 className="font-semibold">Home</h2>
-      </Link>
-      <div className="hidden md:block">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link href="/#about" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  About
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/#projects" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Projects
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/blog?page=1" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Blog
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Contact</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {linksInNav.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-          <NavigationMenuViewport />
-        </NavigationMenu>
-      </div>
-      <div className="flex gap-2">
-        <ModeToggle />
-        <div className="block sm:hidden">
-        <Button onClick={()=>(setShow(!show))} variant={"secondary"}><HamburgerMenuIcon /></Button>
-        <ul className={`${show ? 'flex' : 'hidden' } mt-4 absolute border-box gap-2 text-right text-xl left-0 w-full p-4 flex-col bg-white dark:bg-zinc-950`}>
-          {linksInNavMobile.map((e,i) => (
-            <Link key={i} href={e.href}><li onClick={() => setShow(false)} className="dark:bg-zinc-900 bg-zinc-200  px-4 py-2 rounded-xl">{e.title}</li></Link>
-          ))}
-        </ul>
+  return (
+    <nav
+      className={`w-full flex justify-center sticky z-50 ${
+        visible ? "top-0" : ""
+      } bg-white dark:bg-zinc-950 transition duration-1000 ease-in-out`}
+    >
+      <div className="flex justify-between w-[95%] m-4 p-2 items-center border rounded-lg border-zinc-600">
+        <Link href={"/"} className="text-xl">
+          <h2 className="font-semibold">Home</h2>
+        </Link>
+        <div className="hidden md:block">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/#about" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    About
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/#projects" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Projects
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/blog?page=1" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Blog
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Contact</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-3 lg:w-[600px]">
+                    {linksInNav.map((component) => (
+                      <ListItem
+                        key={component.title}
+                        title={component.title}
+                        href={component.href}
+                      >
+                        {component.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+            <NavigationMenuViewport />
+          </NavigationMenu>
+        </div>
+        <div className="flex gap-2">
+          <ModeToggle />
+          <div className="block sm:hidden">
+            <Button onClick={() => setShow(!show)} variant={"secondary"}>
+              <HamburgerMenuIcon />
+            </Button>
+            <ul
+              className={`${
+                show ? "flex" : "hidden"
+              } mt-4 absolute border-box gap-2 text-right text-xl left-0 w-full p-4 flex-col bg-white dark:bg-zinc-950`}
+            >
+              {linksInNavMobile.map((e, i) => (
+                <Link key={i} href={e.href}>
+                  <li
+                    onClick={() => setShow(false)}
+                    className="dark:bg-zinc-900 bg-zinc-200  px-4 py-2 rounded-xl"
+                  >
+                    {e.title}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
     </nav>
   );
 }
